327139bc4f5db32209e7bc06691add7fac807150
Borys Osypenko <boris_osip@ukr.net>
2018-06-15 12:44:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+let words1 = alphabet[17]  + alphabet[2] + alphabet[20] + alphabet[20] + alphabet[8];
+console.log(words1);
+
+let words2 = alphabet[18]  + alphabet[12] + alphabet[26] + alphabet[12] + alphabet[13] + alphabet[25]  + alphabet[3] + alphabet[7] + alphabet[9] + alphabet[4];
+console.log(words2);
+
+let words3;
+console.log(alphabet[4] + alphabet[3] + alphabet[12] + alphabet[7] + alphabet[28] + alphabet[2]  + alphabet[3]);
